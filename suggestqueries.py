3ca2697f40347687f85ba14f57f215d48abf58c6
sundios <sundios@gmail.com>
2020-11-19 22:10:51 -0800

    change order

--- suggestqueries.py
+++ suggestqueries.py
@@ -7,18 +7,21 @@ import pandas as pd
 
 keyword=input('Add your keyword')
 
+
+
 '''
 api_call makes the first api call to get the initial 10 queries
 We only need one parameter which is the initial Keyword we want 
 to get ideas from. We create a list called Keywords that we store
 all the keywords
 
-Inside this function we also have get_more
+End of function we call 3 other functions to build up the keywords list
 '''
 
 def api_call(keyword):
-    keywords = [keyword]
     
+    keywords = [keyword]
+     
     url = "http://suggestqueries.google.com/complete/search?output=firefox&q=" + keyword
     response = requests.get(url, verify=False)
     suggestions = json.loads(response.text)
@@ -27,12 +30,21 @@ def api_call(keyword):
     for word in suggestions[1]:
         keywords.append(word)
         
-    prefixes(keyword)
-    suffixes(keyword)
-    get_more(keywords)  
+    prefixes(keyword,keywords)
+    suffixes(keyword,keywords)
+    get_more(keywords) 
     
-    
-def prefixes(keyword):
+'''
+prefixes adds a value from the prefix list before the keyword we passed 
+and get suggestions out of that. Then it appends each of the keyword to 
+the keywords lists.
+
+function passes to parameters:
+    keyword: value we want to check.
+    keywords: list to append values.
+'''    
+        
+def prefixes(keyword,keywords):
     
     prefixes = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','y','x','y','z','how','which','why','where','who','when','are','what']    
     
@@ -48,7 +60,17 @@ def prefixes(keyword):
             print(kws[n])
             keywords.append(kws[n])
             
-def suffixes(keyword):
+'''
+suffixes adds a value from the prefix list after the keyword we passed 
+and get suggestions out of that. Then it appends each of the keyword to 
+the keywords lists.
+
+function passes to parameters:
+    keyword: value we want to check.
+    keywords: list to append values.
+'''  
+            
+def suffixes(keyword,keywords):
     
     suffixes =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','y','x','y','z','like','for','without','with','versus','vs','to','near','except','has']
     
@@ -71,6 +93,8 @@ api to get more suggestions. Every new suggestion is stored back in
 the keywords list.
 
 I set a limit of 1000 Keywords but this can be increased.
+Once it hits 1000 keyowrds it stops and we run a deduplicator so we
+ only get unique keywords
 '''
            
 def get_more(keywords):
@@ -82,29 +106,27 @@ def get_more(keywords):
             
             keywords2 =  suggestions[1]
             length = len(keywords2)
+                       
             
             for n in range(length):
                 print(keywords2[n])
                 keywords.append(keywords2[n])
                 print(len(keywords))
-                
-                
-            if len(keywords) >= 2000: #we can increase this number if we want more keywords
+            
+                   
+            if len(keywords) >= 1000: #we can increase this number if we want more keywords
                 print('##Finish here####')
                 break
             
-        keywords = list(dict.fromkeys(keywords))
-    
-    
+        #removing duplicates from the list
+        keywords = list(dict.fromkeys(keywords)) 
+        
         df = pd.DataFrame(keywords,columns=['Keywords'])
         json_hist = df.to_json(orient="table")
 
+        #Exporting all keywords to CSV
         df.to_csv('test.csv')
-
-
-
-
-    
+   
 api_call(keyword)
 
 
