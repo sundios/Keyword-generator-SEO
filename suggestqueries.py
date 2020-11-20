69da965c6d012ed5b7bd4e0c2770c64864f758bc
sundios <sundios@gmail.com>
2020-11-19 21:39:24 -0800

    Added suffix and prefix

--- suggestqueries.py
+++ suggestqueries.py
@@ -17,14 +17,54 @@ Inside this function we also have get_more
 '''
 
 def api_call(keyword):
+    keywords = [keyword]
+    
     url = "http://suggestqueries.google.com/complete/search?output=firefox&q=" + keyword
     response = requests.get(url, verify=False)
     suggestions = json.loads(response.text)
     
-    keywords = suggestions[1]
+    
+    for word in suggestions[1]:
+        keywords.append(word)
         
+    prefixes(keyword)
+    suffixes(keyword)
     get_more(keywords)  
     
+    
+def prefixes(keyword):
+    
+    prefixes = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','y','x','y','z','how','which','why','where','who','when','are','what']    
+    
+    for prefix in prefixes:
+        url = "http://suggestqueries.google.com/complete/search?output=firefox&q=" + prefix + " " + keyword 
+        response = requests.get(url, verify=False)
+        suggestions = json.loads(response.text)
+        
+        kws = suggestions[1]
+        length = len(kws)
+        
+        for n in range(length):
+            print(kws[n])
+            keywords.append(kws[n])
+            
+def suffixes(keyword):
+    
+    suffixes =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','y','x','y','z','like','for','without','with','versus','vs','to','near','except','has']
+    
+    
+    for suffix in suffixes:
+        print(suffix)
+        url = "http://suggestqueries.google.com/complete/search?output=firefox&q=" + keyword + " " + suffix 
+        response = requests.get(url, verify=False)
+        suggestions = json.loads(response.text)
+        
+        kws = suggestions[1]
+        length = len(kws)
+        
+        for n in range(length):
+            print(kws[n])
+            keywords.append(kws[n])   
 '''
 get more takes the keywords list and runs the keywords via the
 api to get more suggestions. Every new suggestion is stored back in
@@ -43,13 +83,13 @@ def get_more(keywords):
             keywords2 =  suggestions[1]
             length = len(keywords2)
             
-            for n in range(1, length):
+            for n in range(length):
                 print(keywords2[n])
                 keywords.append(keywords2[n])
                 print(len(keywords))
                 
                 
-            if len(keywords) >= 3000: #we can increase this number if we want more keywords
+            if len(keywords) >= 2000: #we can increase this number if we want more keywords
                 print('##Finish here####')
                 break
             
